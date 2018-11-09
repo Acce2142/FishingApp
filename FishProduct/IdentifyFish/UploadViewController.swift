329b8dd1686f908db4c45c75489744ad1e99f277
Duoqi Zheng <zhedy005@mymail.unisa.edu.au>
2018-11-09 14:47:22 +1030

    Fix ipad popup window issue

--- FishProduct/IdentifyFish/UploadViewController.swift
+++ FishProduct/IdentifyFish/UploadViewController.swift
@@ -16,10 +16,10 @@ class UploadViewController: UIViewController,UIImagePickerControllerDelegate, UI
     var latitube = ""
     var locationManager: CLLocationManager?
     @IBOutlet weak var FishImage: UIImageView!
+    @IBOutlet weak var photoButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.locationFunc()
-        
         // Do any additional setup after loading the view.
     }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -47,38 +47,7 @@ class UploadViewController: UIViewController,UIImagePickerControllerDelegate, UI
         dateStr = timeFormatter.string(from: date as Date) as String
         timeFormatter.dateFormat = "HH:mm"
         timeStr = timeFormatter.string(from: date as Date) as String
-        Network.sharedTool().IdentifyFish(image: FishImage.image!, date_Str: dateStr, time_Str: timeStr, lon: longtitube, lat: latitube) { (ret) in
-            if ret{
-                //fine
-                let alertController = UIAlertController(title: "Tips",
-                                                        message: "Upload success", preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "OK", style: .default, handler: {
-                    action in
-                    self.navigationController?.popViewController(animated: true)
-                })
-                alertController.addAction(okAction)
-                self.present(alertController, animated: true, completion: nil)
-            }
-            else{
-                //fine
-                let alertController = UIAlertController(title: "Tips",
-                                                        message: "Upload fail", preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "OK", style: .default, handler: {
-                    action in
-                })
-                alertController.addAction(okAction)
-                self.present(alertController, animated: true, completion: nil)
-            }
-        }
-        if(FishImage.image == nil){
-            let alertController = UIAlertController(title: "Tips",
-                                                    message: "Please choose a image first", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default, handler: {
-                action in
-                self.navigationController?.popViewController(animated: true)
-            })
-            alertController.addAction(okAction)
-            self.present(alertController, animated: true, completion: nil)
+        if(FishImage.image != nil){
             switch CLLocationManager.authorizationStatus(){
             case .notDetermined, .restricted, .denied :
                 let alertController = UIAlertController(title: "Tips",
@@ -111,14 +80,25 @@ class UploadViewController: UIViewController,UIImagePickerControllerDelegate, UI
                         self.present(alertController, animated: true, completion: nil)
                     }
                 }
-            }        }
+            }
+            
+        } else {
+            let alertController = UIAlertController(title: "Tips",
+                                                    message: "Please choose a image first", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: {
+                action in
+                self.navigationController?.popViewController(animated: true)
+            })
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
     
     @IBAction func Photo(_ sender: Any) {
         let imagePickerController = UIImagePickerController();
         imagePickerController.delegate = self;
-        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
-        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in
+        var actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+            actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in
             if UIImagePickerController.isSourceTypeAvailable(.camera){
                 imagePickerController.sourceType = .camera
                 self.present(imagePickerController, animated: true, completion: nil)
@@ -135,6 +115,9 @@ class UploadViewController: UIViewController,UIImagePickerControllerDelegate, UI
             self.present(imagePickerController, animated: true, completion: nil)
         }))
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        actionSheet.popoverPresentationController?.sourceView = self.view;
+        actionSheet.popoverPresentationController?.sourceRect = CGRect(x:self.view.frame.size.width / 2 - 40, y:view.frame.height - 90, width: 100, height: 40);
         self.present(actionSheet, animated: true, completion: nil)
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
